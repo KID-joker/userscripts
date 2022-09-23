e1e1b9696a02669bc198bba32832235c4651b238
KID-joker <kid_joker@126.com>
2022-09-23 11:25:35 +0800

    fix: two toolbox

--- scroll-toolbox.js
+++ scroll-toolbox.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         scroll toolbox
 // @namespace    https://github.com/KID-joker/userscript
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL    https://github.com/KID-joker/userscript/blob/main/scroll-toolbox.js
 // @downloadURL  https://github.com/KID-joker/userscript/blob/main/scroll-toolbox.js
 // @supportURL   https://github.com/KID-joker/userscript/issues
